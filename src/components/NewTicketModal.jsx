86f3bab809e0d001f64da200cbdce98cc20ad7d4
Rohan <rohan.2021b0131077@abes.ac.in>
2025-04-26 10:47:22 +0530

    added non editable portal url

--- src/components/NewTicketModal.jsx
+++ src/components/NewTicketModal.jsx
@@ -168,6 +168,21 @@ function NewTicketModal({ onClose, setTickets, setLoading, setError }) {
         </div>
       )}
       
+      <div className="mb-4">
+              <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
+                Portal Url (Read Only): 
+              </label>
+              <input
+                type="text"
+                name="title"
+                value={new URL(document.referrer).hostname}
+                // onChange={handleChange}
+                className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Brief description of the issue"
+                readOnly
+              />
+            </div>
+
       <form id="createTicket" onSubmit={handleSubmit} className="space-y-4">
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
